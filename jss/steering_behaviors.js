5c45dc08b4ec8566c540c958edf75eaec5e6bd09
volkan <volqii@gmail.com>
2021-08-26 14:05:26 +0300

    black

--- jss/steering_behaviors.js
+++ jss/steering_behaviors.js
@@ -57,7 +57,7 @@ function getRandomColor() {
     for (var i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
     }
-    return color;
+    return "#000000";
 }
 
 function windowResized() {
