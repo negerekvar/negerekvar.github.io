2cba22020acb2a1e805f60da54fa0c82963bfe74
volkanozcan2 <vozcan86@googlemail.com>
2021-10-21 21:22:45 +0300

    ?*?

--- jss/steering_behaviors.js
+++ jss/steering_behaviors.js
@@ -3,7 +3,7 @@ var offset;
 var points;
 var vehicles = [];
 var _text = "merhaba_dünya";
-var colors=["f72585","b5179e","7209b7","560bad","480ca8","3a0ca3","3f37c9","4361ee","4895ef","4cc9f0"]
+
 
 console.log("merhaba dünya!!!")
 
--- jss/vehicle.js
+++ jss/vehicle.js
@@ -7,7 +7,7 @@ function Vehicle(x, y) {
     this.maxspeed = 10;
     this.maxforce = 1;
     this.rad = random(5, 10);
-    this.color = random(colors);
+    this.color = random(["f72585","b5179e","7209b7","560bad","480ca8","3a0ca3","3f37c9","4361ee","4895ef","4cc9f0"]);
 }
 
 
